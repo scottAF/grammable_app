57dd97830ddc7b4e4d2fcecbf4ed4ea8706b4784
Scott Ford <uofoduck14@gmail.com>
2019-06-26 23:23:42 +0000

    fixed HTML 302 status while running rspec test

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -13,6 +13,9 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should return a 404 error if the gram is not found" do
+      user = FactoryBot.create(:user)
+      sign_in user
+      
       get :show, params: { id: 'TACOCAT' }
       expect(response).to have_http_status(:not_found)
     end
